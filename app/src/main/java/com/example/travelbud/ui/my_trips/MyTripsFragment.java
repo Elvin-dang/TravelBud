ba08f9346b2b5e429de056649fea9902e6b4c4b4
mhy12345678 <113097771+mhy12345678@users.noreply.github.com>
2022-10-11 00:58:30 +1100

    added sample destination activity
    todo: link all of them

--- app/src/main/java/com/example/travelbud/ui/my_trips/MyTripsFragment.java
+++ app/src/main/java/com/example/travelbud/ui/my_trips/MyTripsFragment.java
@@ -1,45 +1,53 @@
 package com.example.travelbud.ui.my_trips;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.travelbud.R;
-import com.example.travelbud.TransitCardsAdapter;
-import com.example.travelbud.databinding.FragmentDestinationsBinding;
-
+import com.example.travelbud.TravelBudUser;
+import com.example.travelbud.adapter.TransitCardsAdapter;
+import com.example.travelbud.adapter.TripCardsAdapter;
+import com.example.travelbud.databinding.FragmentMyTripsBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MyTripsFragment extends Fragment {
 
-    private FragmentDestinationsBinding binding;
+    private FragmentMyTripsBinding binding;
     private View transit_card;
     private DatabaseReference mDatabase;
-
+    public TravelBudUser current_user;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        MyTripsViewModel homeViewModel =
+        MyTripsViewModel myTripsViewModel =
                 new ViewModelProvider(this).get(MyTripsViewModel.class);
 
-        binding = FragmentDestinationsBinding.inflate(inflater, container, false);
+        binding = FragmentMyTripsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        homeViewModel.getDestinations().observe(getViewLifecycleOwner(), destinations -> {
-            RecyclerView rv = (RecyclerView) root.findViewById(R.id.destinations_rv);
+
+        myTripsViewModel.getUser("John").observe(getViewLifecycleOwner(), user -> {
+            current_user = user;
+            RecyclerView rv = (RecyclerView) root.findViewById(R.id.trips_rv);
             rv.setHasFixedSize(true);
             LinearLayoutManager llm = new LinearLayoutManager(root.getContext());
             rv.setLayoutManager(llm);
-            TransitCardsAdapter adapter = new TransitCardsAdapter(destinations);
+            TripCardsAdapter adapter = new TripCardsAdapter(user.getTrips());
             rv.setAdapter(adapter);
         });
 
